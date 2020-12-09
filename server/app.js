a0179c23c8f1908ba2a0659b38c8a7f17a5b0917
Joshua Barringer <barrinjo@oregonstate.edu>
2020-12-08 20:24:09 -0800

    create garden working with postman

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 import indexRouter from './routes/index'
 import usersRouter from './routes/userRoutes'
+import gardenRouter from './routes/gardenRoutes'
 var app = express()
 app.use(logger('dev'))
 app.use(express.json())
@@ -13,4 +14,5 @@ app.use(cookieParser())
 // setup API routes
 app.use('/', indexRouter)
 app.use('/user', usersRouter)
+app.use('/garden', gardenRouter)
 export default app
