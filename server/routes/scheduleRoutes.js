51e43f57112a138e3395e9209c9dd7a1078d3cf0
Joshua Barringer <barrinjo@oregonstate.edu>
2021-02-21 12:20:59 -0800

    small route fix

--- server/routes/scheduleRoutes.js
+++ server/routes/scheduleRoutes.js
@@ -7,7 +7,7 @@ router.post('/', ScheduleController.newSchedule)
 // get one schedule by schedule_id
 router.get('/:schedule_id', ScheduleController.getScheduleByID)
 // get all schedules associated with a user
-router.post('/user/:user_id', ScheduleController.getAllSchedules)
+router.get('/user/:user_id', ScheduleController.getAllSchedules)
 // update schedule
 router.put('/:schedule_id', ScheduleController.updateSchedule)
 // delete schedule by id
