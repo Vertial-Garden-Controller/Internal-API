0a329a52d73d8e8177cc83e9930e6125a4723aef
Joshua Barringer <barrinjo@oregonstate.edu>
2021-02-21 11:08:28 -0800

    added: garden controllers and routes

--- server/controllers/gardenController.js
+++ server/controllers/gardenController.js
@@ -48,4 +48,169 @@ export default class GardenController {
     // if success, return garden's id
     return res.status(201).json({ success: true, garden_id: garden_id })
   }
+
+  /**
+   * Handles GET requets for garden information by garden_id.
+   * Garden_id provided must be greater than zero and be a valid number,
+   * otherwise status 400.
+   * Res contains success status and garden information upon success.
+   * @param req
+   * @param res
+   * @returns res with json
+   */
+  static async getGardenByID(req, res) {
+    const garden_id = parseInt(req.params.garden_id)
+    if (garden_id < 1 || isNaN(garden_id)) {
+      return res.status(400).json({
+        success: false,
+        error: 'Request Error',
+        detail: 'Garden id provided is invalid',
+      })
+    }
+
+    // TODO: service for getGardenByID
+    const garden = await GardenService.getGardenByID(garden_id)
+    if (garden.error) {
+      return res.status(500).json({
+        success: false,
+        error: garden.error,
+        detail: garden.detail,
+      })
+    }
+
+    return res.status(200).json({ success: true, garden: garden })
+  }
+
+  /**
+   * Handles GET requets for all gardens of a given user
+   * Garden_id provided must be greater than zero and be a valid number,
+   * otherwise status 400.
+   * Res contains success status and garden information upon success.
+   * @param req
+   * @param res
+   * @returns res with json
+   */
+  static async getAllGardens(req, res) {
+    const user_id = parseInt(req.params.user_id)
+    if (user_id < 1 || isNaN(user_id)) {
+      return res.status(400).json({
+        success: false,
+        error: 'Request Error',
+        detail: 'User id provided is invalid',
+      })
+    }
+
+    // TODO: service for getAllGardens
+    const gardens = await GardenService.getAllGardens(user_id)
+    if (gardens.error) {
+      return res.status(500).json({
+        success: false,
+        error: gardens.error,
+        detail: gardens.detail,
+      })
+    }
+
+    return res.status(200).json({ success: true, gardens: gardens })
+  }
+
+  /**
+   * Updates an existing garden in the database.  Must provide an existing garden_id
+   * and garden information in body.garden.  Will return existing garden
+   * information upon success.
+   * @param req
+   * @param res
+   * @returns res with json
+   */
+  static async updateGarden(req, res) {
+    const garden_id = parseInt(req.params.garden_id)
+    if (garden_id < 1 || isNaN(garden_id)) {
+      return res.status(400).json({
+        success: false,
+        error: 'Request Error',
+        detail: 'Garden id provided is invalid',
+      })
+    }
+
+    // check for existing garden to update
+    const existingGarden = await GardenService.getGardenByID(garden_id)
+    // if garden does not exist, return error.
+    if (!existingGarden) {
+      return res.status(400).json({
+        success: false,
+        error: 'Request Error',
+        detail: `Garden #${garden_id} does not exist and cannot be updated.`,
+      })
+    }
+
+    // request body contains garden signup information
+    const newGarden = req.body.garden
+    // return 400 (bad request) if firstname or email is missing.
+    if (
+      !newGarden.start_time ||
+      !newGarden.end_time ||
+      !newGarden.days_active ||
+      !newGarden.garden_id
+    ) {
+      return res.status(400).json({
+        success: false,
+        error: 'Request Error',
+        detail:
+          'New garden requires days_active, start_time, and end_time, and garden_id',
+      })
+    }
+
+    // Create update old garden to existing garden
+    // TODO: service for updateGarden
+    const garden = await GardenService.updateGarden(garden_id, garden)
+    if (garden.error) {
+      return res.status(500).json({
+        success: false,
+        error: garden.error,
+        detail: garden.detail,
+      })
+    }
+
+    return res.status(200).json({ success: true, garden: garden })
+  }
+
+  /**
+   * Looks up a garden at garden_id and deletes it from the database.
+   * If no garden at that garden_id exists, then an error will be returned.
+   * @param req
+   * @param res
+   * @returns res with json
+   */
+  static async deleteGarden(req, res) {
+    const garden_id = parseInt(req.params.garden_id)
+    if (garden_id < 1 || isNaN(garden_id)) {
+      return res.status(400).json({
+        success: false,
+        error: 'Request Error',
+        detail: 'Garden id provided is invalid',
+      })
+    }
+
+    // check for existing garden to update
+    const existingGarden = await GardenService.getGardenByID(garden_id)
+    // if garden does not exist, return error.
+    if (!existingGarden) {
+      return res.status(400).json({
+        success: false,
+        error: 'Request Error',
+        detail: `Garden #${garden_id} does not exist and cannot be deleted.`,
+      })
+    }
+
+    // TODO: service for deleteGarden
+    const garden = await GardenService.deleteGarden(garden_id)
+    if (garden.error) {
+      return res.status(500).json({
+        success: false,
+        error: garden.error,
+        detail: garden.detail,
+      })
+    }
+
+    return res.status(200).json({ success: true })
+  }
 }
--- server/routes/gardenRoutes.js
+++ server/routes/gardenRoutes.js
@@ -2,6 +2,15 @@ import express from "express";
 var router = express.Router();
 import GardenController from "../controllers/gardenController";
 
+// create a new garden for a user
 router.post('/new', GardenController.createNewGarden)
+// create a new garden for a user
+router.post('/new', GardenController.getGardenByID)
+// create a new garden for a user
+router.post('/new', GardenController.getAllGardens)
+// create a new garden for a user
+router.post('/new', GardenController.updateGarden)
+// create a new garden for a user
+router.post('/new', GardenController.deleteGarden)
 
 export default router;
